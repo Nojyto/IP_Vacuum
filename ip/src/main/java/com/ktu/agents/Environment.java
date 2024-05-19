425e4f03a7344415b28a81c2948f9ebe69fec6ff
SnowyRat <persis.aivaras@gmail.com>
2024-05-19 13:35:59 +0300

    randomas jibanas

--- ip/src/main/java/com/ktu/agents/Environment.java
+++ ip/src/main/java/com/ktu/agents/Environment.java
@@ -5,6 +5,6 @@ import jade.core.Agent;
 public class Environment extends Agent {
     @Override
     protected void setup() {
-
+        //Nelabai megstu negrusiu, bet cia reikia prideti agento elgsena
     }
 }
